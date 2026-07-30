9a7a9815b330e03af0e643a8a6298013d51f940b
Davit Napitupulu <davit.s.na70@gmail.com>
2026-07-30 23:12:10 +0700

    fix(test): import drift companion value helper

--- test/data/report_repository_test.dart
+++ test/data/report_repository_test.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pharmacy_inventory_platform/data/database.dart';
